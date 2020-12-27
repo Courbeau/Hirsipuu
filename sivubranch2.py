2b365ff0221e92eec04013d4e975ba1939bea6c8
Aaro <37548092+Courbeau@users.noreply.github.com>
2020-12-27 11:47:30 +0200

    Update sivubranch2.py

--- sivubranch2.py
+++ sivubranch2.py
@@ -1,6 +1,8 @@
 import random
 
-lista = [("k", "i", "s", "s", "a"), "jalkapallo", "asunto"]
+uusisana = ""
+
+lista = [("k", "i", "s", "s", "a"), ("j", "a", "l", "k", "a", "p", "a", "l", "l", "o"), "asunto"]
 hidword = ""
 
 a = random.randint(0, 2)
@@ -16,26 +18,21 @@ sanavaikirjain = input("Haluatko arvata sanaa vai kirjainta? 0 jos sanaa, 1 jos 
 
 if sanavaikirjain == "0":
     sana_arvaus = input("Arvaa sanaa: ")
-    if sana_arvaus == sana:
-        print("Oikein meni, voitit!")
-    else:
-        print("Väärin meni")
+
+    #tää kohta
 
 if sanavaikirjain == "1":
     kirjain_arvaus = input("Arvaa kirjainta: ")
+    if kirjain_arvaus in lista[0]:
+        for kirjain in lista[0]:
+            if kirjain_arvaus == kirjain:
+                uusisana = uusisana + kirjain
+            else:
+                uusisana = uusisana + "_"
+    else:
+        print("Ei ole")
 
-    if sana.find(kirjain_arvaus) > -1:
-        hidword
-        
-        
-uusisana = ""
 
-if kirjain_arvaus in lista[0]:
-    for kirjain in lista[0]:
-        if kirjain_arvaus == kirjain:
-            uusisana = uusisana + kirjain
-        else:
-            uusisana = uusisana + "_"
-else:
-    print("Ei ole")
+print(uusisana)
+
 
